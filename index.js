dfb65872675d828906b0dd160e5db0f5c889ef70
Vanessa <vanessa.co403@gmail.com>
2023-02-05 21:46:31 -0500

    objects lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Vanessa Coelho",
+    streetAddress: "123 Skyler St."
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key) {
+    const { [key]: removed, ...newEmployee } = employee;
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
